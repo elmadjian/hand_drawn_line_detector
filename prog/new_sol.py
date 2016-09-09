45d09e096e2bf60b0ebbda97434381cde70cebd9
elmadjian <cadu_elmadjian@yahoo.com.br>
2016-09-09 13:18:47 -0300

    still not finding the paths, but the graph is ok now

--- prog/new_sol.py
+++ prog/new_sol.py
@@ -1,4 +1,4 @@
-import sys, cv2, queue
+import sys, cv2, heapq, itertools
 import numpy as np
 from datetime import datetime
 
@@ -15,16 +15,16 @@ class Graph():
     def set_img(self, img):
         self.img = img
 
-    def set_seed(self, seed):
-        self.seed = Node(seed)
-        self.__mark_node(seed)
-        self.V += 1
+    # def set_seed(self, seed):
+    #     self.seed = Node(seed)
+    #     self.__mark_node(seed)
+    #     self.V += 1
 
     def set_arch(self, node_A, node_B):
         node_A.set_arch(node_B)
         self.node[node_A.pixel] = node_A
         self.node[node_B.pixel] = node_B
-        self.__mark_node(node_B.pixel)
+        self._mark_node(node_B.pixel)
         self.V += 2
         self.N += 1
 
@@ -62,17 +62,17 @@ class Graph():
                 return self.node[p]
         return False
 
-    def __mark_node(self, pixel):
-        self.img[pixel] = (0,255,0)
+    def _mark_node(self, pixel):
+        self.img[pixel] = (0,100,0)
 
 
 
 #===============================================================================
 class Node():
-    def __init__(self, pixel):
+    def __init__(self, pixel, cost=sys.maxsize):
         self.arch = set()
         self.pixel = pixel
-        self.time = datetime.now()
+        self.cost = cost
 
     def has_arch(self):
         if len(self.arch) > 0:
@@ -92,13 +92,40 @@ class Node():
         return self.pixel[1]
 
     def __lt__(self, other):
-        self.time < other.time
+        self.cost < other.cost
+
+#===============================================================================
+
+class PriorityQueue:
+    def __init__(self):
+        self.queue = []
+        self.entry_finder = {}
+        self.counter = itertools.count()
+
+    def put(self, item, priority):
+        if item in self.entry_finder:
+            self.remove(item)
+        entry = [priority, next(self.counter), item]
+        self.entry_finder[item] = entry
+        heapq.heappush(self.queue, entry)
+
+    def remove(self, item):
+        entry = self.entry_finder.pop(item)
+        entry[-1] = "removed"
+
+    def pop(self):
+        while self.queue:
+            priority, count, item = heapq.heappop(self.queue)
+            if item != "removed":
+                del self.entry_finder[item]
+                return item
+        raise KeyError('pop from an empty priority queue')
 
 #===============================================================================
 
 MAX_INT = sys.maxsize
 G = Graph()
-Q = queue.PriorityQueue()
+Q = PriorityQueue()
 
 
 
@@ -124,38 +151,33 @@ def main():
 
     pos = seeds[0]
     G.set_img(img)
-    G.set_seed(pos)
-    Q.put((-1, G.seed))
+    Q.put(Node(pos, 0), 0)
+    neighborhood = get_neighborhood(4)
     sink_count = len(sinks)
 
-    #while sink_count != 0:
-    for i in range(10):
-        lowest = Q.get()[1]
-        print("lowest:", lowest.pixel)
-        neighbor = G.has_4_neighbor(lowest.pixel)
-        if neighbor:
-            G.set_arch(lowest, neighbor)
-
-        neighborhood_4_cost(gray, lowest)
-
-        #TODO
-        #Escrever um codigo para mostrar o grafo, com as setinhas e tudo o mais
-        print("arco:", G.get_node(lowest).get_arch())
+    while sink_count != 0:
+    #for i in range(30000):
+        lowest = Q.pop()
+        G.add_node(lowest)
+        ift(gray, lowest, neighborhood)
 
         if lowest.pixel in sinks:
             sink_count -= 1
 
-        cv2.imshow("teste", img)
-        k = cv2.waitKey(0)
-        if k & 0xFF == ord('q'):
-            sys.exit()
+        # cv2.imshow("teste", img)
+        # k = cv2.waitKey(0)
+        # if k & 0xFF == ord('q'):
+        #     sys.exit()
 
-    view_paths(img, sinks)
-    # cv2.imshow("teste", img)
-    # k = cv2.waitKey(0)
-    # if k & 0xFF == ord('q'):
-    #     sys.exit()
-    # cv2.destroyAllWindows()
+    for s in sinks:
+        view_path(img, s)
+    #view_path(img, sinks[6])
+
+    cv2.imshow("teste", img)
+    k = cv2.waitKey(0)
+    if k & 0xFF == ord('q'):
+        sys.exit()
+    cv2.destroyAllWindows()
 
 # Get a window centered on pixel p of size (2*k + 1)^2
 #-----------------------------------------------------
@@ -167,47 +189,47 @@ def get_window(img, p, k):
 
 # Get local minimum of adjacency
 #--------------------------------
-def neighborhood_4_cost(img, node):
-    adj_list = [(-1,0), (0,1), (1,0), (0,-1)]
-    for i in adj_list:
-        pixel = (node.y() + i[0], node.x() + i[1])
-        if not G.has_node(pixel):
-            n = Node(pixel)
-            G.add_node(n)
-            cost = abs(img[pixel] - img[node.pixel]) + img[pixel]
-            Q.put((cost, n))
+def get_neighborhood(neighborhood):
+    if neighborhood == 4:
+        return [(-1,0), (0,1), (1,0), (0,-1)]
+    if neighborhood == 8:
+        return [(-1,-1), (-1,0), (-1,1), (0,1), (1,1), (1,0), (1,-1), (0,-1)]
+
 
 #---------------------------------
-def neighborhood_8_cost(img, node):
-    adj_list = [(-1,-1), (-1,0), (-1,1), (0,1), (1,1), (1,0), (1,-1), (0,-1)]
-    for i in adj_list:
+def ift(img, node, neighborhood):
+    for i in neighborhood:
         pixel = (node.y() + i[0], node.x() + i[1])
+        if not is_valid_pixel(img, pixel):
+            continue
         if not G.has_node(pixel):
             n = Node(pixel)
-            G.add_node(n)
             cost = abs(img[pixel] - img[node.pixel]) + img[pixel]
-            Q.put((cost, n))
+            if cost < n.cost:
+                if n.cost != sys.maxsize:
+                    Q.remove(n)
+                n.cost = cost
+                G.set_arch(n, node)
+                Q.put(n, cost)
 
 #-------------------------
-# def count():
-#     if not hasattr(count, "counter"):
-#         count.counter = 0
-#     count.counter += 1
-#     return count.counter
+def is_valid_pixel(img, pixel):
+    y = pixel[0]
+    x = pixel[1]
+    if y >= 0 and y < img.shape[0]:
+        if x >= 0 and x < img.shape[1]:
+            return True
+    return False
 
-#------------------------------
-def view_paths(img, sinks):
-    for s in sinks:
-        node = G.get_node(s)
-        while (node.has_arch()):
-            img[node.pixel] = (0,255,255)
-            #print(len(node.arch))
-            node = node.get_arch()
 
-    cv2.imshow("teste", img)
-    k = cv2.waitKey(0)
-    if k & 0xFF == ord('q'):
-        sys.exit()
+#Shows a path backwards, stating from sink
+#-----------------------------------------
+def view_path(img, sink):
+    node = G.get_node(sink)
+    while (node.has_arch()):
+        img[node.pixel] = (0,255,255)
+        node = node.get_arch()
+
 
 
 # Open an image
